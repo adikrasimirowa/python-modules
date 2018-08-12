664216d0e3a2b42e4e6ab30e6a22cc3480bc7ce3
Adi <abitodorova@gmail.com>
2018-08-13 01:22:21 +0300

    Add second function f_x

--- solution.py
+++ solution.py
@@ -1,5 +1,11 @@
 def f_c(x):
-	# y = x*0+4
-	return 4 #y
+	return 4
 """ Returns the constant 4 for any input parameter. """
 print (f_c(3)) # x = 3 is set as an example. 3 can be changed to another number
+
+def f_x(x, a, b):
+        f = a*x + b
+        return f
+print (f_x(3,2,3))
+
+
