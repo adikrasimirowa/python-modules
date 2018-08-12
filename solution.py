aa7d030bdbcbf6a795d6fb239ffdc83f1673c087
Adi <abitodorova@gmail.com>
2018-08-13 01:26:05 +0300

    Add third function sum

--- solution.py
+++ solution.py
@@ -8,4 +8,9 @@ def f_x(x, a, b):
         return f
 print (f_x(3,2,3))
 
-
+def sum(x):
+        s=0
+        for i in range(1,4):     
+                s += f_x(x, i, i)
+        return s
+print (sum(3))
