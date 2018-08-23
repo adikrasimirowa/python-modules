3f909b2a2003094e43f347e4594e46de3b265b17
Adelina <adelina@athlonsofia.com>
2018-08-24 02:21:27 +0300

    Add module2

---
+++ module2/solution.py
@@ -0,0 +1,42 @@
+def num_add(a, b):
+        return a + b
+
+def num_sub(a, b):
+	return a - b
+
+def num_mul(a, b):
+	return a * b
+
+def num_div(a, b):
+	return a / b
+
+def num_floor(a, b):
+	return a // b
+
+def num_rem(a, b):
+	return a % b
+
+IS_TRUE = True
+IS_FALSE = False
+
+PANCAKE_INGREDIENTS = {'flour': 2, 'eggs': 4, 'milk': 200, 'butter': False, 'salt': 0.001 }
+
+def ingredient_exists(ingr, dict):
+	for ingr in dict:
+	    if ingr in dict:
+	       return True
+	    else:
+	        return False
+
+def flatten_pancakes(dict):
+	 coppy = dict.copy()
+	
+if __name__ == '__main__':
+	print(num_add(2, 3))
+	print(num_sub(3, 5))
+	print(num_mul(3, 5))
+	print(num_div(3, 5))
+	print(num_floor(3, 5))
+	print(num_rem(3, 5))
+
+
